17ad4a7caca2c3f060fc41fca8449760d041f146
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2021-06-05 19:37:52 +0000

    Update (#1)
    * Update to typescript
    * Update dependencies

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Slingercode
 
 This repo is made with inspiration from [leerob](https://github.com/leerob).
+
+---
+
+[Wiki](https://github.com/slingercode/_slingercode/wiki)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
