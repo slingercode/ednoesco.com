2cfd69844586d9ae487f80c43fa8c1fc533dde80
Noel Espino Córdova <noelespinocordova@tutanota.com>
2021-07-25 21:42:46 -0500

    Add Event component

--- pages/timeline/index.jsx
+++ pages/timeline/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import Container from '../../components/Container';
+import Event from '../../components/timeline/event';
+import { getDatabase } from '../../lib/notion';
 
-const Timeline = () => {
-  return <div />;
+const Timeline = ({ events }) => {
+  return (
+    <Container title="Slingercode - timeline">
+      <div className="grid md:grid-flow-row md:grid-cols-3 gap-3">
+        {events.map((event) => (
+          <Event
+            key={event.id}
+            name={event.properties.Name.title[0].plain_text}
+            description={event.properties.Description.rich_text[0].plain_text}
+            date={event.properties.Date.date.start}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export const getStaticProps = async () => {
+  const sorts = [
+    {
+      property: 'Date',
+      direction: 'ascending',
+    },
+  ];
+
+  const database = await getDatabase(
+    process.env.NOTION_TABLE_TIMELINE_ID,
+    sorts
+  );
+
+  return {
+    props: {
+      events: database,
+    },
+    revalidate: 10,
+  };
 };
 
 export default Timeline;
