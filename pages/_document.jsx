4fe8c4c90c089189429363791183f0c1d59f2e0a
Noel Espino Córdova <noelespinocordova@tutanota.com>
2021-09-11 13:04:41 -0500

    Add og:image property

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,7 +5,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* <meta property="og:title" content="This is the title" /> */}
+          {/* <meta property="og:type" content="This is the title" /> */}
+          {/* <meta property="og:url" content="This is the title" /> */}
+          <meta property="og:image" content="/imgs/demo.jpg" />
+        </Head>
 
         <body className="bg-gray-background-main text-gray-font-high">
           <Main />
