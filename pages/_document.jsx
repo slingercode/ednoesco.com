fd1b75f9614b8137433b0875ff2df5191128c14a
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2021-09-11 18:58:52 +0000

    Og meta (#7)
    Integrate Open Graph meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,7 +5,33 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Open Graph */}
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="slingercode" />
+          <meta
+            property="og:description"
+            content="Welcome! To my personal page"
+          />
+          <meta
+            property="og:image"
+            content="https://cdn.discordapp.com/attachments/461523220219297796/886323680433569832/demo_1.jpeg"
+          />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:site" content="@_slingercode" />
+          <meta property="twitter:creator" content="@_slingercode" />
+          <meta property="twitter:title" content="slingercode" />
+          <meta
+            property="twitter:description"
+            content="Welcome! To my personal page"
+          />
+          <meta
+            property="twitter:image"
+            content="https://cdn.discordapp.com/attachments/461523220219297796/886323680433569832/demo_1.jpeg"
+          />
+        </Head>
 
         <body className="bg-gray-background-main text-gray-font-high">
           <Main />
