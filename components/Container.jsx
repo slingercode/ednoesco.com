865d21144303a54f9d9087617dd9f82f085a4b86
Noel Espino Córdova <noelespinocordova@tutanota.com>
2021-09-11 13:18:23 -0500

    og img server

--- components/Container.jsx
+++ components/Container.jsx
@@ -9,7 +9,10 @@ const Container = ({ title, children }) => (
       {/* Open Graph */}
       <meta property="og:title" content="slingercode" />
       <meta property="og:description" content="Esto es la descripción" />
-      <meta property="og:image" content="/imgs/demo.jpg" />
+      <meta
+        property="og:image"
+        content="https://cdn.discordapp.com/attachments/473563114986536972/839346414768619540/1612541918766.jpg"
+      />
       <meta property="og:type" content="website" />
 
       {/* Twitter */}
