5d6afd43d108a85262a35b06e000e7a003f8cf54
Noel Espino Córdova <noelespinocordova@tutanota.com>
2021-09-19 13:36:48 -0500

    Add revalidate and new og banner

---
+++ components/common/HeaderMenuItem.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
+const HeaderMenuItem = ({ link, label }) => (
+  <DropdownMenu.Item className="p-1 rounded-md cursor-pointer focus-visible:outline-none focus-visible:ring focus-visible:border-gray-border-interactive focus-visible:ring-gray-border-interactive focus-visible:rounded-md hover:bg-gray-background-hover">
+    <Link href={link}>
+      <a className="flex">{label}</a>
+    </Link>
+  </DropdownMenu.Item>
+);
+
+export default HeaderMenuItem;
--- pages/tweets/tech.jsx
+++ pages/tweets/tech.jsx
@@ -29,6 +29,7 @@ export const getStaticProps = async () => {
       tweets: tweets,
       errors: errors,
     },
+    revalidate: 10,
   };
 };
 
