245b7163114f9e0b42ce33e727c5a44b6fdaad31
Noel Espino Córdova <noelespinocordova@tutanota.com>
2021-09-26 20:00:56 -0500

    WIP: about

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,15 @@
+import Link from 'next/link';
 import * as Separator from '@radix-ui/react-separator';
 
 const Footer = () => (
   <footer className="p-5 md:p-10 lg:px-28 lg:py-10 ">
     <Separator.Root className="h-px bg-gray-border-non-interactive" />
+
+    <div className="flex justify-center mt-4">
+      <Link href="/about">
+        <a className="text-gray-font-low">about</a>
+      </Link>
+    </div>
   </footer>
 );
 
