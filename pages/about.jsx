ca83aa5b802baef117d5345f18900e816f24d283
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2021-09-27 03:20:14 +0000

    Min blogs (#9)
    * Blogs index
    * First refactor events
    * WIP: about
    * Add Blogs page component
    * Update notion env names
    * Fix properties from otaku and blogs
    * Min fix
    * Fix media on tweets
    * Add meme tweets
    * Fix title tab

---
+++ pages/about.jsx
@@ -0,0 +1,83 @@
+import Link from 'next/link';
+
+import Container from '../components/Container';
+
+const About = () => {
+  return (
+    <Container title="slingercode - about">
+      <div>
+        <h2>WIP: This module is not ready yet</h2>
+
+        <br />
+
+        <h2>Cool Tech 🤯</h2>
+
+        <p>This is what i use to build this website 🙃</p>
+
+        <ul className="list-disc list-inside">
+          <li>
+            <a
+              href="https://vercel.com"
+              target="_blank"
+              className="text-blue-font-low"
+            >
+              Vercel
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://nextjs.org"
+              target="_blank"
+              className="text-blue-font-low"
+            >
+              Next.js
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://tailwindcss.com"
+              target="_blank"
+              className="text-blue-font-low"
+            >
+              Tailwind
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.radix-ui.com"
+              target="_blank"
+              className="text-blue-font-low"
+            >
+              Radix UI
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://developers.notion.com"
+              target="_blank"
+              className="text-blue-font-low"
+            >
+              Notion API
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://omg.lol"
+              target="_blank"
+              className="text-blue-font-low"
+            >
+              OMG.LOL
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  );
+};
+
+export default About;
---
+++ pages/blogs/index.jsx
@@ -0,0 +1,36 @@
+import Container from '../../components/Container';
+import Article from '../../components/blogs/Article';
+import usePosts from '../../hooks/blogs/usePosts';
+import { getDatabase } from '../../lib/notion';
+
+const Index = ({ posts }) => {
+  const { data } = usePosts(posts);
+
+  return (
+    <Container title="slingercode - blogs">
+      <div className="grid gap-4">
+        {data.map((post) => (
+          <Article
+            key={post.id}
+            id={post.id}
+            title={post.properties.Name.title[0].text.content}
+            status={post.properties.Status.select.name}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(process.env.NOTION_BLOGS);
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 10,
+  };
+};
+
+export default Index;
