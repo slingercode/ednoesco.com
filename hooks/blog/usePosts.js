e6689769c112fa103595552ae5a6dcb98b5d6b12
Noel Espino Córdova <noelespinocordova@tutanota.com>
2021-10-16 19:43:03 +0000

    I18n (#10)
    * Add: i18n on blogs and basic validations
    * Posible fix?
    * Add: i18n to blogs and warnings
    * Console.error
    * Update to next-translate
    * Remove links from otaku and blogs
    * Add: i18n to go back legend
    * Update route and add blogs to index

---
+++ hooks/blog/usePosts.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const usePosts = (posts = [], locale) => {
+  const mapping = {
+    'en-US': 'English',
+    'es-MX': 'Spanish',
+  };
+
+  const [localPosts, setLocalPost] = useState([]);
+
+  useEffect(() => filterByLanguage(posts, locale), [posts, locale]);
+
+  const filterByLanguage = (_posts, _locale) => {
+    const filtered = _posts.filter(
+      (post) => mapping[_locale] === post.properties.Language.select.name
+    );
+
+    return setLocalPost(filtered);
+  };
+
+  return { data: localPosts };
+};
+
+export default usePosts;
