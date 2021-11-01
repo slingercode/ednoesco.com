320edf98ce62909df16698432ba3811aebdc9e91
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2021-11-01 00:56:05 +0000

    Add Twitter media icon (#11)

---
+++ components/twitter/Errors.jsx
@@ -0,0 +1,9 @@
+import Warning from '../common/Warning';
+
+const Errors = () => (
+  <div className="mb-4">
+    <Warning>Some tweets not longer exists 😕</Warning>
+  </div>
+);
+
+export default Errors;
