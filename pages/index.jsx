988bacb2d8401351e34983f0e4bbb3d097b2c349
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-01-10 09:57:08 -0600

    First iteration of index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import * as Separator from '@radix-ui/react-separator';
 
 import Container from '../components/Container';
@@ -11,6 +12,45 @@ const Index = ({ blog }) => {
 
   return (
     <Container>
+      <div className="md:px-10 flex">
+        <div className="w-4/6 flex flex-col justify-center">
+          <h1 className="text-3xl md:text-5xl">Noel Espino</h1>
+
+          <div className="pt-2">
+            <h2 className="text-md md:text-xl lg:text-2xl text-gray-font-low">
+              {'React Developer at '}
+              <span>
+                <a
+                  href="https://twitter.com/GrainChainIO"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  @GrainChainIO
+                </a>
+              </span>
+            </h2>
+          </div>
+        </div>
+
+        <div className="w-2/6 flex justify-center items-center">
+          <Image src="/me.png" width={150} height={150} />
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <h3 className="text-xl text-center">
+          Hi! Welcome to my little space on the wide internet 😁
+        </h3>
+
+        <div className="pt-4">
+          <p>
+            This is my personal web page in which i want to leave my digital
+            fingerprint (a healthy one), share my hobbies and start writing some
+            of my interests as a remainder on how I'm improving 🙂
+          </p>
+        </div>
+      </div>
+
       {!!data.length && (
         <div>
           <div className="w-full">
