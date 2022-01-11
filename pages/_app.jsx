ade7c48a127dca218ab988fd284a3b476584f277
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-01-11 16:29:47 +0000

    Update dependencies (#12)
    * Update Nextjs to v11
    * Update Nextjs to v12
    * Update Tailwindcss to v3
    * Update Radixui primitives
    * Update notion api and next-translate

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,11 @@
-import { IdProvider } from '@radix-ui/react-id';
-
 import AppContainer from '../components/AppContainer';
 
 import 'tailwindcss/tailwind.css';
 
 const Slingercode = ({ Component, pageProps }) => (
-  <IdProvider>
-    <AppContainer>
-      <Component {...pageProps} />
-    </AppContainer>
-  </IdProvider>
+  <AppContainer>
+    <Component {...pageProps} />
+  </AppContainer>
 );
 
 export default Slingercode;
