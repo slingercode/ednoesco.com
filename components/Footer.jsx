7170ad6c3917aa08fe942c8ab10a544f92e7cef8
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2022-03-18 08:54:06 -0600

    Add patch notes screen

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,10 +5,16 @@ const Footer = () => (
   <footer className="p-5 md:p-10 lg:px-28 lg:py-10 ">
     <Separator.Root className="h-px bg-gray-border-non-interactive" />
 
-    <div className="flex justify-center mt-4">
+    <div className="mt-4 flex-col sm:flex sm:flex-row text-center sm:mx-60 sm:justify-between">
       <Link href="/about" locale="en-US">
         <a className="text-gray-font-low">about</a>
       </Link>
+
+      <div>
+        <Link href="/patch-notes" locale="en-US">
+          <a className="text-gray-font-low">patch notes</a>
+        </Link>
+      </div>
     </div>
   </footer>
 );
