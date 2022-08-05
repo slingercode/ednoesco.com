4179b831970df522ca83bcd029ddfbdc8e951ec4
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2022-08-05 14:11:23 -0500

    Add github public pgp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,10 @@ function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </main>
 
-      <footer className="mt-10 mb-5 flex justify-center text-mauve-low">
+      <footer className="mt-10 mb-5 flex flex-col justify-center items-center text-yellow-high">
+        <Text className="text-xs font-mono">
+          07AB 633F B4CF D35D 4468 4895 0345 F1CA 0F3A D813
+        </Text>
         <Text className="text-sm">{`MIT License; @_slingercode`}</Text>
       </footer>
     </>
