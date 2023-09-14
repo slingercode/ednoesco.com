7829a9faa46f0efd9f6a64e95b1c70de0b884c5c
Noel Espino Córdova <github@ednoesco.com>
2023-09-14 03:30:31 +0000

    Add blogs & refactor astro link (#23)

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    draft: z.boolean(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
