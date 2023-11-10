e37d4634155150b6ea69d2304a61c3c105a6b34a
Noel Espino Córdova <github@ednoesco.com>
2023-11-09 18:48:10 -0600

    Add ViewTransitions and Prefetch APIs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
+  experimental: {
+    contentCollectionCache: true,
+  },
+  prefetch: {
+    prefetchAll: false,
+  },
   integrations: [tailwind()],
 });
