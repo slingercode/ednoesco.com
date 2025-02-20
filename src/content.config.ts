e98ef5e2e240b3b13d78c4b6d7de6e86f1f325ce
Noel Espino Córdova <github@ednoesco.com>
2025-02-19 19:11:18 -0600

    Update Astro 5.3

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: '**.md', base: './src/content/blog' }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
